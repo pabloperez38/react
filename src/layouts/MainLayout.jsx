df7eedfa13c29eca25c3881ae9992cc9612a2f6c
pabloperez38 <pablo.eluniversoweb@gmail.com>
2025-11-16 20:06:37 -0300

    Arreglo footer

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -1,5 +1,6 @@
 import { Outlet } from "react-router-dom";
 import { Nav } from "../components/Nav/Nav";
+import { Footer } from "../components/Footer/Footer";
 
 export const MainLayout = () => {
     return (
